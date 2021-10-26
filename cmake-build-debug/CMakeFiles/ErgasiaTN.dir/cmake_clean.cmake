30d7f4ac6af38637f9a52ed460a3083a0082a910
Anna G. Zosan <56229337+annagzosan@users.noreply.github.com>
2021-10-26 14:50:00 +0300

    Add files via upload

---
+++ cmake-build-debug/CMakeFiles/ErgasiaTN.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ErgasiaTN.dir/Game.cpp.obj"
+  "CMakeFiles/ErgasiaTN.dir/main.cpp.obj"
+  "ErgasiaTN.exe"
+  "ErgasiaTN.exe.manifest"
+  "ErgasiaTN.pdb"
+  "libErgasiaTN.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ErgasiaTN.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
